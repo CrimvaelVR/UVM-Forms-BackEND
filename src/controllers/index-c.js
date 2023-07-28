c19caacb67e1b7110228eec816e0a9e0e1ccdd16
JoseEscalona18 <114792259+JoseEscalona18@users.noreply.github.com>
2023-07-28 08:17:38 -0400

    Algunos cambios

--- src/controllers/index-c.js
+++ src/controllers/index-c.js
@@ -3,7 +3,7 @@ const forms = require('../models/formularios');
 class indexController {
     consultarFormularioUser = async (req, res) => {
         try {
-          const id_Autor = "64bdc96bb35effdbf71156f9"
+          const id_Autor = "64c3ace76b00be0b6a971bc4"
           const formularios = await forms.find({id_Autor: id_Autor})
           const user = 'Jose Escalona'
           res.status(201).render('index-usuarios', {user: user, formularios: formularios})
