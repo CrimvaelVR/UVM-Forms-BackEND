e1db0ed78d4bffe2fc01cbc1ad51d8aa5deb5f55
CrimvaelVR <maikelvr2004@gmail.com>
2023-07-28 13:36:05 -0400

    Ahora la cookie se maneja en unas funciones

--- src/controllers/formularios-c.js
+++ src/controllers/formularios-c.js
@@ -8,37 +8,80 @@ class formularioController {
   crearFormulario = async (req, res) => {
     try {
       const datos = req.body;
-      const {titulo, descripcion} = req.body
-      const id_Autor = "esca123456"
-      const formularios = await forms.find()
-      let ultimoId
-
-      if (formularios == ""){
-        ultimoId = 1
-      }else{
-        const ultimaPregunta = formularios[formularios.length-1].preguntas
-        ultimoId = ultimaPregunta[ultimaPregunta.length-1].id + 1
-      }
-
-      const preguntasObj = Object.keys(datos)
-      .filter(clave => clave.startsWith("pregunta"))
-      .map(clave => datos[clave]);
-    
-      const preguntas = preguntasObj.map((pregunta, index) => ({
-        id: index + ultimoId,
-        texto: pregunta,
-        tipo: "",
-        opciones: "",
-      }));
-
-      const nuevoformulario = new forms({id_Autor, titulo, descripcion, preguntas});
-      await nuevoformulario.save();
-      res.status(201).render({mensaje: 'Formulario creado correctamente', usuario: nuevoformulario});
-      
+      const {titulo, descripcion} = req.body;
+  
+      // Obtener el token de la cookie
+      const token = req.cookies.token;
+      // Decodificar el token sin verificar su firma
+      const decoded = jwt.decode(token);
+      // Obtener el nombre del autor del token
+      const nombreAutor = decoded.usuario;
+  
+      // Buscar al autor por su nombre en la base de datos
+      user.findOne({usuario: nombreAutor})
+        .then(autor => {
+          // Si se encuentra al autor, obtener su id
+          if (autor) {
+            const id_Autor = autor._id;
+  
+            // Buscar los formularios existentes para obtener el último id
+            forms.find()
+              .then(formularios => {
+                let ultimoId;
+  
+                if (formularios == ""){
+                  ultimoId = 1;
+                }else{
+                  const ultimaPregunta = formularios[formularios.length-1].preguntas;
+                  ultimoId = ultimaPregunta[ultimaPregunta.length-1].id + 1;
+                }
+  
+                // Crear un array de preguntas a partir de los datos del formulario
+                const preguntasObj = Object.keys(datos)
+                  .filter(clave => clave.startsWith("pregunta"))
+                  .map(clave => datos[clave]);
+                
+                const preguntas = preguntasObj.map((pregunta, index) => ({
+                  id: index + ultimoId,
+                  texto: pregunta,
+                  tipo: "",
+                  opciones: "",
+                }));
+  
+                // Crear un nuevo formulario con el id del autor, el título, la descripción y las preguntas
+                const nuevoformulario = new forms({id_Autor, titulo, descripcion, preguntas});
+                // Guardar el nuevo formulario en la base de datos
+                nuevoformulario.save()
+                  .then(formulario => {
+                    // Enviar una respuesta con el mensaje y el formulario creados
+                    res.status(201).render({mensaje: 'Formulario creado correctamente', usuario: formulario});
+                  })
+                  .catch(error => {
+                    // Si ocurre un error al guardar el formulario, enviar un mensaje de error
+                    console.error(error);
+                    res.status(500).json({mensaje: 'Error al guardar el formulario', error});
+                  });
+              })
+              .catch(error => {
+                // Si ocurre un error al buscar los formularios, enviar un mensaje de error
+                console.error(error);
+                res.status(500).json({mensaje: 'Error al buscar los formularios', error});
+              });
+          } else {
+            // Si no se encuentra al autor, enviar un mensaje de error
+            res.status(404).json({mensaje: 'Autor no encontrado'});
+          }
+        })
+        .catch(error => {
+          // Si ocurre un error al buscar al autor, enviar un mensaje de error
+          console.error(error);
+          res.status(500).json({mensaje: 'Error al buscar al autor', error});
+        });
     } catch (error) {
       res.status(500).json({mensaje: 'Error al crear el formulario', error});
     }
   };
+  
 
   obtenerFormularioId = async (req, res) => {
     try {
--- src/controllers/index-c.js
+++ src/controllers/index-c.js
@@ -1,17 +1,51 @@
 const forms = require('../models/formularios');
+const user = require('../models/usuarios')
+const jwt = require('jsonwebtoken')
+
 
 class indexController {
-    consultarFormularioUser = async (req, res) => {
-        try {
-          const id_Autor = "64c3ace76b00be0b6a971bc4"
-          const formularios = await forms.find({id_Autor: id_Autor})
-          const user = 'Jose Escalona'
-          res.status(201).render('index-usuarios', {user: user, formularios: formularios})
-        } catch (error) {
-          res.status(500).json({mensaje: 'Error al crear el formulario', error});
-        }
-    };
+  consultarFormularioUser = async (req, res) => {
+    try {
+      // Obtener el token de la cookie
+      const token = req.cookies.token;
+      // Decodificar el token sin verificar su firma
+      const decoded = jwt.decode(token);
+      // Obtener el nombre de usuario del token
+      const username = decoded.usuario;
 
+      // Buscar al usuario por su nombre de usuario en la base de datos
+      user.findOne({usuario: username})
+        .then(usuario => {
+          // Si se encuentra al usuario, obtener su id
+          if (usuario) {
+            const id_Autor = usuario._id;
+            console.log(id_Autor)
+            // Buscar los formularios que tengan el id del usuario como autor
+            forms.find({id_Autor: id_Autor})
+              .then(formularios => {
+                // Renderizar la vista con los datos del usuario y los formularios
+                res.status(200).render('index-usuarios', {user: username, formularios: formularios});
+              })
+              .catch(error => {
+                // Si ocurre un error al buscar los formularios, enviar un mensaje de error
+                console.error(error);
+                res.status(500).json({mensaje: 'Error al buscar los formularios', error});
+              });
+          } else {
+            // Si no se encuentra al usuario, enviar un mensaje de error
+            res.status(404).json({mensaje: 'Usuario no encontrado'});
+          }
+        })
+        .catch(error => {
+          // Si ocurre un error al buscar al usuario, enviar un mensaje de error
+          console.error(error);
+          res.status(500).json({mensaje: 'Error al buscar al usuario', error});
+        });
+    } catch (error) {
+      // Si ocurre un error al crear el formulario, enviar un mensaje de error
+      res.status(500).json({mensaje: 'Error al crear el formulario', error});
+    }
+  };
 }
 
 const indexC = new indexController();
